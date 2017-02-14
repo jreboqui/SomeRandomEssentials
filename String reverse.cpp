68fb0181998d57d7bece0fa5cb07e4f201986237
Junnel Reboquio <jreboqui@ucsd.edu>
2017-02-14 08:08:16 -0800

    Reversing a string method a

---
+++ String reverse.cpp
@@ -0,0 +1,12 @@
+//There's many ways of doing this...
+//given a string
+
+String str; 
+//did something to store a string in str
+
+for (int i = str.length()-1; i >= 0; i--)
+              {
+                  std::cout << str[i] << " ";
+              }
+              std::cout << std::endl;
+    
